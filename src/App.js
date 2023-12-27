6d4610be7d54ddb4c78b3fcf1bbe97ada64a12a4
mounika.neerati <mounika.neerati93@gmail.com>
2023-12-27 12:11:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      
+   
+    <h1 className="text-4xl font-bold underline text-center text-green-600 bg-purple-300 ">Hello world!</h1>
+    <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+      <center>
+        <h1 className='text-2xl'>Welcome to tailwindcss</h1>
+      </center>
+    </div>
+  )
+}
+
+export default Hello
